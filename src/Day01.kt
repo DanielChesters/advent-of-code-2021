84e5b1a425cdfbef11b2f4d1e5bb83cb7d42e268
Daniel Chesters <daniel@coin-coin.xyz>
2022-01-04 21:33:47 +0100

    Day 01 - Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,9 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputInt = input.map(String::toInt)
+            return (1 until input.size).map {
+                inputInt[it-1] < inputInt[it]
+            }.count { it }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +12,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
